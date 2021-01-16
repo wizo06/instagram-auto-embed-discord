3abc25dd774638737fd578c3a8c374f18b01f8ef
wizo <14183421+wizo06@users.noreply.github.com>
2021-01-15 22:19:48 -0500

    hotfix attempt

--- src/app.js
+++ src/app.js
@@ -57,7 +57,21 @@ const loginIfNeeded = (page) => {
 
 (async () => {
   logger.debug('Launching puppeteer.');
-  const browser = await puppeteer.launch({ headless: true, defaultViewport: { width: 800, height: 1440 } });
+  const browser = await puppeteer.launch({ 
+    headless: true, 
+    defaultViewport: { width: 800, height: 1440 },
+    ignoreHTTPSErrors: true,
+    userDataDir: './tmp',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-infobars',
+      '--window-position=0,0',
+      '--ignore-certifcate-errors',
+      '--ignore-certifcate-errors-spki-list',
+      '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+    ]
+  });
   const page = await browser.newPage();
   await loginIfNeeded(page);
   await page.close();
