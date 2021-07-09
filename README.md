c6fe0882e37e1eae56d07d04f958eb90d14230ff
wizo06 <14183421+wizo06@users.noreply.github.com>
2021-07-09 00:17:59 -0400

    2.0

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # instagram-auto-embed-discord
 Discord bot for auto embedding instagram links
 
+# Motivation
+There was a period of time where discord wouldn't auto embed for instagram links.
+
+As of the publish date of version `2.0` of this bot, discord suddenly started auto embedding instagram links again. *What are the odds...*
+
+Therefore, this repo will be archived.
+
 # Getting Started
 Discord permissions
 - View Channels
 - Send Messages
+- Embed Links
 
 ```
-git clone https://github.com/wizo06/instagram-auto-embed-discord.git
-cd instagram-auto-embed-discord
-cp config/template.toml config/user_config.toml
+cp config/config-template.toml config/config.toml
 npm i
 npm start
 ```
--- src/app.js
+++ src/app.js
@@ -1,101 +1,77 @@
-const path = require('path');
-const discord = require('discord.js');
-const puppeteer = require('puppeteer-extra');
-const logger = require('logger');
-require('toml-require').install({ toml: require('toml') });
+const cheerio = require('cheerio')
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+const logger = require('logger')
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
+const config = require('@iarna/toml').parse(require('fs').readFileSync('config/config.toml'))
 
-const CONFIG = require(path.join(process.cwd(), 'config/user_config.toml'));
+const client = new Discord.Client()
 
-const BOT = new discord.Client({ retryLimit: Infinity });
+client.on('ready', () => {
+  logger.success(`Logged in as ${client.user.tag}!`)
+})
 
-const loginIfNeeded = (page) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      logger.debug('Navigating to @instagram');
-      await page.goto('https://www.instagram.com/instagram/');
-      await page.waitForTimeout(CONFIG.instagram.navigationTimeout);
+client.on('message', async msg => {
+  if (msg.content.match(/^https:\/\/(www\.)?instagram\.com\/p\//)) {
+    logger.info(msg.content)
+    
+    const options = {
+      headers: {
+        'Cookie': config.instagram.cookie
+      }
+    }
+    const res = await fetch(msg.content, options)
+    const body = await res.text()
+    const $ = cheerio.load(body)
 
-      let link = await page.url();
-      if (link.match(/https:\/\/(www\.)*instagram\.com\/accounts\/login/)) {
-        logger.debug('Landed on login page');
+    const embed = new Discord.MessageEmbed()
+      .setColor('#E1306C')
 
-        logger.debug('Input username');
-        await page.type('input[name="username"]', CONFIG.instagram.username);
-        logger.debug('Input password');
-        await page.type('input[name="password"]', CONFIG.instagram.password);
-        logger.debug('Click Login button');
-        await page.click('button.L3NKy');
-        await page.waitForTimeout(CONFIG.instagram.navigationTimeout);
+    $('script').each(function (i, elem) {
+      if ($(this)['0'].children[0]?.data.startsWith('window.__additionalDataLoaded')) {
+        const func = $(this)['0'].children[0]?.data
+        const extractedJson = func.replace(/(^window\.__additionalDataLoaded\('(\/|[0-9A-z]|-)+',)|(\);$)/g, '')
+        const json = JSON.parse(extractedJson)
+
+        const author = `${json.graphql.shortcode_media.owner.full_name} (${json.graphql.shortcode_media.owner.username})`
         
-        // logger.debug('Taking screenshot');
-        // await page.screenshot({path: 'login.png'});
-
-        link = await page.url();
-        if (link.match(/https:\/\/(www\.)*instagram\.com\/accounts\/onetap\//)) {
-          logger.debug('Landed on OneTap page');
-          
-          await page.click('button.yWX7d');
-          await page.waitForTimeout(CONFIG.instagram.navigationTimeout);
-
-          // logger.debug('Taking screenshot');
-          // await page.screenshot({ path: 'onetap.png' });
-        }
-        logger.debug('Login successful');
-
-        // logger.debug('Taking screenshot');
-        // await page.screenshot({ path: 'loggedin.png' });
+        embed
+          .setImage(json.graphql.shortcode_media.display_url)
+          .setDescription(json.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text)
+          .setAuthor(author, json.graphql.shortcode_media.owner.profile_pic_url, 'https://instagram.com/tsai_919')
+          .setFooter('Instagram', 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
       }
-      else {
-        logger.debug('Login not needed');
+    })  
+
+    return await msg.channel.send(embed)
+  }
+
+  if (msg.content.match(/^https:\/\/(www\.)?instagram\.com\/[\w]+\/?$/)) {
+    logger.info(msg.content)
+    
+    const options = {
+      headers: {
+        'Cookie': config.instagram.cookie
       }
-
-      resolve();
     }
-    catch (e) {
-      logger.error('Login failed');
-      reject();
-    }
-  });
-};
+    const res = await fetch(msg.content, options)
+    const body = await res.text()
+    const $ = cheerio.load(body)
+    
+    const embed = new Discord.MessageEmbed()
+      .setColor('#E1306C')
+    
+    $('meta').each(function (i, elem) {
+      const property = $(this)['0'].attribs.property
+      const val = $(this)['0'].attribs.content
+      if (property === 'og:title') embed.setTitle(val)
+      if (property === 'og:url') embed.setURL(val)
+      if (property === 'og:image') embed.setThumbnail(val)
+      if (property === 'og:description') embed.setDescription(val)
 
-(async () => {
-  logger.debug('Launching puppeteer.');
-  const browser = await puppeteer.launch({ 
-    args: ['--no-sandbox'],
-    headless: false, 
-    defaultViewport: { width: 800, height: 1440 },
-  });
-  const page = await browser.newPage();
-  
-  await loginIfNeeded(page).catch(e => process.exit(1));
-  await page.close();
-  logger.debug('Puppeteer launched successfully for instagram auto embed.');
+    })
+    return await msg.channel.send(embed)
+  }
+})
 
-  BOT.on('message', async msg => {
-    if (msg.content.match(/https:\/\/(www\.)*instagram\.com\/.+/)) {
-      logger.debug('Instagram link detected');
-      require(path.join(process.cwd(), `src/autoEmbed/instagram.js`)).run(msg, browser);
-    }
-  });
-
-  BOT.on('error', err => {
-    logger.error(err.message);
-  });
-
-  BOT.on('ready', async () => {
-    logger.info(`Logged in as ${BOT.user.tag}`);
-    logger.info(`**********************************************`);
-
-    // const channel = await BOT.channels.fetch(CONFIG.discord.channelID);
-    // await channel.send({files: [
-    //   {attachment: 'login.png'},
-    //   {attachment: 'onetap.png'},
-    //   {attachment: 'loggedin.png'}
-    // ]});
-  });
-
-  BOT.login(CONFIG.discord.token).catch(e => logger.error(e));
-})();
+client.login(config.discord.token)
