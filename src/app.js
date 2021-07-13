5585574d45a774f6238bb8c27085930275c1a93c
wizo06 <14183421+wizo06@users.noreply.github.com>
2021-07-13 00:01:46 -0400

    Fixed link for posts

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ client.on('message', async msg => {
   const arr = msg.content.split(' ')
   const filtered = arr.filter(ele => ele.match(/^https:\/\/(www\.)?instagram\.com/))
   for (const link of filtered) {
-    logger.info(link)
     const urlObj = new URL(link)
     const normalizedLink = `${urlObj.origin}${urlObj.pathname}`
+    logger.info(normalizedLink)
     if (normalizedLink.match(/^https:\/\/(www\.)?instagram\.com\/p\//)) {
       const options = {
         headers: {
@@ -32,29 +32,18 @@ client.on('message', async msg => {
         .setColor('#E1306C')
         .setFooter('Instagram', 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
 
-      // $('script').each(function (i, elem) {
-      //   if ($(this)['0'].children[0]?.data.startsWith('window.__additionalDataLoaded')) {
-      //     const func = $(this)['0'].children[0]?.data
-      //     const extractedJson = func.replace(/(^window\.__additionalDataLoaded\('(\/|[0-9A-z]|-)+',)|(\);$)/g, '')
-      //     const json = JSON.parse(extractedJson)
-  
-      //     const author = `${json.graphql.shortcode_media.owner.full_name} (${json.graphql.shortcode_media.owner.username})`
+      $('script').each(function (i, elem) {
+        if ($(this)['0'].children[0]?.data.startsWith('window.__additionalDataLoaded')) {
+          const func = $(this)['0'].children[0]?.data
+          const extractedJson = func.replace(/(^window\.__additionalDataLoaded\('(\/|[0-9A-z]|-)+',)|(\);$)/g, '')
+          const json = JSON.parse(extractedJson)
+          const author = `${json.graphql.shortcode_media.owner.full_name} (${json.graphql.shortcode_media.owner.username})`
           
-      //     embed
-      //       .setImage(json.graphql.shortcode_media.display_url)
-      //       .setDescription(json.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text)
-      //       .setAuthor(author, json.graphql.shortcode_media.owner.profile_pic_url, 'https://instagram.com/tsai_919')
-      //       .setFooter('Instagram', 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
-      //   }
-      // })  
-
-      $('meta').each(function (i, elem) {
-        const property = $(this)['0'].attribs.property
-        const val = $(this)['0'].attribs.content
-        if (property === 'og:title') embed.setTitle(val)
-        if (property === 'og:url') embed.setURL(val)
-        if (property === 'og:image') embed.setImage(val)
-        if (property === 'og:description') embed.setDescription(val)
+          embed
+            .setImage(json.graphql.shortcode_media.display_url)
+            .setDescription(json.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text)
+            .setAuthor(author, json.graphql.shortcode_media.owner.profile_pic_url, `https://www.instagram.com/${json.graphql.shortcode_media.owner.username}`)
+        }
       })
   
       return await msg.channel.send(embed)
@@ -85,7 +74,6 @@ client.on('message', async msg => {
 
       return await msg.channel.send(embed)
     }
-    
   }
 })
 
