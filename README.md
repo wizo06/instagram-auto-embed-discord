241f6b9ddabc8c7f12ab3b119065f113163138ce
wizo06 <14183421+wizo06@users.noreply.github.com>
2021-07-12 23:18:35 -0400

    Now embeds multiple links from same message

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # instagram-auto-embed-discord
 Discord bot for auto embedding instagram links
 
-# Motivation
-There was a period of time where discord wouldn't auto embed for instagram links.
-
-As of the publish date of version `2.0` of this bot, discord suddenly started auto embedding instagram links again. *What are the odds...*
-
-Therefore, this repo will be archived.
-
 # Getting Started
 Discord permissions
 - View Channels
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const cheerio = require('cheerio')
 const Discord = require('discord.js')
 const fetch = require('node-fetch')
-const logger = require('logger')
+const logger = require('@wizo06/logger')
 
 const config = require('@iarna/toml').parse(require('fs').readFileSync('config/config.toml'))
 
@@ -12,65 +12,80 @@ client.on('ready', () => {
 })
 
 client.on('message', async msg => {
-  if (msg.content.match(/^https:\/\/(www\.)?instagram\.com\/p\//)) {
-    logger.info(msg.content)
-    
-    const options = {
-      headers: {
-        'Cookie': config.instagram.cookie
+  const arr = msg.content.split(' ')
+  const filtered = arr.filter(ele => ele.match(/^https:\/\/(www\.)?instagram\.com/))
+  for (const link of filtered) {
+    logger.info(link)
+    const urlObj = new URL(link)
+    const normalizedLink = `${urlObj.origin}${urlObj.pathname}`
+    if (normalizedLink.match(/^https:\/\/(www\.)?instagram\.com\/p\//)) {
+      const options = {
+        headers: {
+          'Cookie': config.instagram.cookie
+        }
       }
+      const res = await fetch(normalizedLink, options)
+      const body = await res.text()
+      const $ = cheerio.load(body)
+  
+      const embed = new Discord.MessageEmbed()
+        .setColor('#E1306C')
+        .setFooter(val, 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
+
+      // $('script').each(function (i, elem) {
+      //   if ($(this)['0'].children[0]?.data.startsWith('window.__additionalDataLoaded')) {
+      //     const func = $(this)['0'].children[0]?.data
+      //     const extractedJson = func.replace(/(^window\.__additionalDataLoaded\('(\/|[0-9A-z]|-)+',)|(\);$)/g, '')
+      //     const json = JSON.parse(extractedJson)
+  
+      //     const author = `${json.graphql.shortcode_media.owner.full_name} (${json.graphql.shortcode_media.owner.username})`
+          
+      //     embed
+      //       .setImage(json.graphql.shortcode_media.display_url)
+      //       .setDescription(json.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text)
+      //       .setAuthor(author, json.graphql.shortcode_media.owner.profile_pic_url, 'https://instagram.com/tsai_919')
+      //       .setFooter('Instagram', 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
+      //   }
+      // })  
+
+      $('meta').each(function (i, elem) {
+        const property = $(this)['0'].attribs.property
+        const val = $(this)['0'].attribs.content
+        if (property === 'og:title') embed.setTitle(val)
+        if (property === 'og:url') embed.setURL(val)
+        if (property === 'og:image') embed.setImage(val)
+        if (property === 'og:description') embed.setDescription(val)
+      })
+  
+      return await msg.channel.send(embed)
     }
-    const res = await fetch(msg.content, options)
-    const body = await res.text()
-    const $ = cheerio.load(body)
-
-    const embed = new Discord.MessageEmbed()
-      .setColor('#E1306C')
-
-    $('script').each(function (i, elem) {
-      if ($(this)['0'].children[0]?.data.startsWith('window.__additionalDataLoaded')) {
-        const func = $(this)['0'].children[0]?.data
-        const extractedJson = func.replace(/(^window\.__additionalDataLoaded\('(\/|[0-9A-z]|-)+',)|(\);$)/g, '')
-        const json = JSON.parse(extractedJson)
-
-        const author = `${json.graphql.shortcode_media.owner.full_name} (${json.graphql.shortcode_media.owner.username})`
-        
-        embed
-          .setImage(json.graphql.shortcode_media.display_url)
-          .setDescription(json.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text)
-          .setAuthor(author, json.graphql.shortcode_media.owner.profile_pic_url, 'https://instagram.com/tsai_919')
-          .setFooter('Instagram', 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
+  
+    if (normalizedLink.match(/^https:\/\/(www\.)?instagram\.com\/[\w]+\/?$/)) {      
+      const options = {
+        headers: {
+          'Cookie': config.instagram.cookie
+        }
       }
-    })  
+      const res = await fetch(normalizedLink, options)
+      const body = await res.text()
+      const $ = cheerio.load(body)
+      
+      const embed = new Discord.MessageEmbed()
+        .setColor('#E1306C')
+        .setFooter(val, 'https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico')
+      
+      $('meta').each(function (i, elem) {
+        const property = $(this)['0'].attribs.property
+        const val = $(this)['0'].attribs.content
+        if (property === 'og:title') embed.setTitle(val)
+        if (property === 'og:url') embed.setURL(val)
+        if (property === 'og:image') embed.setThumbnail(val)
+        if (property === 'og:description') embed.setDescription(val)
+      })
 
-    return await msg.channel.send(embed)
-  }
-
-  if (msg.content.match(/^https:\/\/(www\.)?instagram\.com\/[\w]+\/?$/)) {
-    logger.info(msg.content)
-    
-    const options = {
-      headers: {
-        'Cookie': config.instagram.cookie
-      }
+      return await msg.channel.send(embed)
     }
-    const res = await fetch(msg.content, options)
-    const body = await res.text()
-    const $ = cheerio.load(body)
     
-    const embed = new Discord.MessageEmbed()
-      .setColor('#E1306C')
-    
-    $('meta').each(function (i, elem) {
-      const property = $(this)['0'].attribs.property
-      const val = $(this)['0'].attribs.content
-      if (property === 'og:title') embed.setTitle(val)
-      if (property === 'og:url') embed.setURL(val)
-      if (property === 'og:image') embed.setThumbnail(val)
-      if (property === 'og:description') embed.setDescription(val)
-
-    })
-    return await msg.channel.send(embed)
   }
 })
 
